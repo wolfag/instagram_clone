478e1a00f56302befeedddfdce7d57493662ef22
TaiNguyen <tai@useget.com>
2020-08-09 11:46:37 +0700

    refactor DatePickekerField

--- src/components/DatePickerField.tsx
+++ src/components/DatePickerField.tsx
@@ -2,22 +2,26 @@ import {FormikProps, FormikValues} from 'formik';
 import React from 'react';
 import {StyleProp, StyleSheet, Text, View, ViewStyle} from 'react-native';
 import DatePicker from 'react-native-datepicker';
+import moment from 'moment';
 import {styles as commonStyles} from './styles';
+import {DATE_FORMAT} from '../constants';
 
 export interface DatePickerFieldProps {
   name: string;
   error?: string;
   style?: StyleProp<ViewStyle>;
   showError?: boolean;
+  format?: string;
 }
 
 const DatePickerField = ({
   name,
   values,
-  handleChange,
+  setFieldValue,
   setFieldTouched,
   style,
   showError = true,
+  format = DATE_FORMAT,
   ...rest
 }: DatePickerFieldProps & FormikProps<FormikValues>) => {
   const {errors, touched} = rest;
@@ -26,9 +30,10 @@ const DatePickerField = ({
       <DatePicker
         date={values[name]}
         onDateChange={(date: string) => {
-          handleChange(name)(date);
+          setFieldValue(name, new Date(moment(date, format).toString()), true);
           setFieldTouched(name, true, true);
         }}
+        format={format}
         {...rest}
       />
       {showError && touched && touched[name] && errors && errors[name] ? (
