e924d4aa6bf185c8189b795a6dad7eaf99282c56
TaiNguyen <tai@useget.com>
2020-08-09 09:33:23 +0700

    changeUsernameForm

---
+++ src/screens/Auth/components/ChangeUsernameForm.tsx
@@ -0,0 +1,67 @@
+import {Formik, FormikHelpers, FormikProps, FormikValues} from 'formik';
+import React from 'react';
+import {Text, View} from 'react-native';
+import * as yup from 'yup';
+import Button from '../../../components/Button';
+import Input from '../../../components/Input';
+import {styles as parentStyle} from '../Welcome';
+
+export interface ChangeUsernameFormValue {
+  username: string;
+}
+
+export interface ChangeUsernameFormProps {
+  onSubmit: (
+    values: ChangeUsernameFormValue,
+    formikHelpers: FormikHelpers<ChangeUsernameFormValue>,
+  ) => void | Promise<any>;
+}
+
+const ChangeUsernameForm = ({
+  onSubmit,
+  username,
+}: ChangeUsernameFormProps & ChangeUsernameFormValue): JSX.Element => {
+  const initialValues: ChangeUsernameFormValue = {username};
+  const validationSchema = yup.object().shape(
+    {
+      username: yup.string().label('Username').required(),
+    },
+    ['username'],
+  );
+
+  return (
+    <Formik
+      validateOnBlur={true}
+      validateOnChange={true}
+      onSubmit={onSubmit}
+      validationSchema={validationSchema}
+      initialValues={initialValues}>
+      {(formProps: FormikProps<FormikValues>) => {
+        const {isValid, dirty, handleSubmit} = formProps;
+        return (
+          <View style={parentStyle.content}>
+            <View>
+              <Text style={parentStyle.title}>CHANGE USERNAME</Text>
+            </View>
+            <Text style={parentStyle.description}>
+              Pick username for your account. You can always change it later.
+            </Text>
+            <Input
+              name="username"
+              autoCapitalize="none"
+              placeholder="Username"
+              {...formProps}
+            />
+            <Button
+              onPress={handleSubmit}
+              disabled={!isValid || !dirty}
+              label="Next"
+            />
+          </View>
+        );
+      }}
+    </Formik>
+  );
+};
+
+export default ChangeUsernameForm;
