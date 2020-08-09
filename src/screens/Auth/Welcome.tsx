92fec653d321e53b7fc0cef1afb7f78e602374e6
TaiNguyen <tai@useget.com>
2020-08-09 09:35:43 +0700

    welcome

--- src/screens/Auth/Welcome.tsx
+++ src/screens/Auth/Welcome.tsx
@@ -1,22 +1,139 @@
-import React, {useState} from 'react';
-import * as yup from 'yup';
-import {View, SafeAreaView, Text, TouchableOpacity} from 'react-native';
+import {RouteProp} from '@react-navigation/native';
+import {StackNavigationProp} from '@react-navigation/stack';
+import React, {useCallback, useState} from 'react';
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import Button from '../../components/Button';
+import Loading from '../../components/Loading';
+import {SCREEN_HEIGHT, STATUS_BAR_HEIGHT} from '../../constants';
+import {CommonParamList} from '../../navigation/RootTab';
+import ChangeUsernameForm from './components/ChangeUsernameForm';
+import {RegisterFormValueStep1} from './components/RegisterFormStep1';
+import {RegisterFormValueStep2} from './components/RegisterFormStep2';
+import {RegisterFormValueStep3} from './components/RegisterFormStep3';
 
-const WelcomeScreen = (): JSX.Element => {
+export type RegisterFormValue = RegisterFormValueStep1 &
+  RegisterFormValueStep2 &
+  RegisterFormValueStep3;
+
+type WelcomeScreenRouteProp = RouteProp<CommonParamList, 'WelcomeScreen'>;
+type WelcomeScreenNavigationProp = StackNavigationProp<
+  CommonParamList,
+  'WelcomeScreen'
+>;
+type WelcomeScreenProps = {
+  navigation: WelcomeScreenNavigationProp;
+  route: WelcomeScreenRouteProp;
+};
+
+type WelcomeContentProps = {
+  username: string;
+  onNext: () => void;
+  onChangeUsername: () => void;
+};
+
+const WelcomeContent = ({
+  username,
+  onNext,
+  onChangeUsername,
+}: WelcomeContentProps): JSX.Element => {
+  return (
+    <View style={styles.content}>
+      <View>
+        <Text style={styles.title}>WELCOME TO INSTAGRAM</Text>
+        <Text style={styles.username}>{`@${username}`}</Text>
+      </View>
+      <Text style={styles.description}>
+        {
+          'Find people to follow and start sharing photos.\nYou can change you username anytime.'
+        }
+      </Text>
+      <Button onPress={onNext} label="Next" />
+      <Button
+        onPress={onChangeUsername}
+        link
+        label="Change username"
+        labelStyle={styles.changeBtnText}
+      />
+    </View>
+  );
+};
+
+const WelcomeScreen = ({
+  navigation,
+  route,
+}: WelcomeScreenProps): JSX.Element => {
   const [loading, setLoading] = useState<boolean>(false);
-  const [hidePassword, setHidePassword] = useState<boolean>(true);
-  const [username, setUsername] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
+  const [isChangeUsername, setIsChangeUsername] = useState<boolean>(false);
+  const [username, setUsername] = useState(
+    route?.params?.formData?.email?.split('@')[0],
+  );
+
+  const _onChangeUsername = useCallback(() => {
+    setIsChangeUsername(true);
+  }, []);
+
+  const _onNext = useCallback(() => {}, []);
 
   return (
-    <SafeAreaView>
-      <View>
-        <TouchableOpacity>
-          <Text>welcome</Text>
-        </TouchableOpacity>
+    <SafeAreaView style={styles.container}>
+      <Loading loading={loading} label="Registering..." />
+      <View style={styles.body}>
+        {isChangeUsername ? (
+          <ChangeUsernameForm onSubmit={() => {}} username={username} />
+        ) : (
+          <WelcomeContent
+            onNext={_onNext}
+            onChangeUsername={_onChangeUsername}
+            username={username}
+          />
+        )}
+      </View>
+
+      <View style={styles.footer}>
+        <Text>
+          <Text style={styles.footerText}>
+            By clicking Next, you agree to our{' '}
+            <Text style={styles.emphasisText}>
+              Terms, Data Policy and Cookies Policy
+            </Text>
+          </Text>
+        </Text>
       </View>
     </SafeAreaView>
   );
 };
 
 export default WelcomeScreen;
+
+const styles = StyleSheet.create({
+  container: {backgroundColor: '#fff'},
+  body: {
+    marginHorizontal: 20,
+  },
+  content: {
+    width: '100%',
+    height: SCREEN_HEIGHT - STATUS_BAR_HEIGHT - 100,
+    justifyContent: 'center',
+    alignItems: 'center',
+    zIndex: 1,
+  },
+  footer: {
+    height: 100,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 25,
+  },
+  title: {fontWeight: '600', textAlign: 'center'},
+  username: {fontWeight: '600', textAlign: 'center'},
+  description: {color: '#666', textAlign: 'center', marginVertical: 10},
+  changeBtnText: {color: '#318bfb'},
+  footerText: {
+    fontSize: 12,
+    fontWeight: '500',
+    color: '#666',
+    textAlign: 'center',
+  },
+  emphasisText: {color: '#000'},
+});
+
+export {styles};
